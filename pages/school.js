20f6f6544a452cad2a97a8fc9f25b3cda3fda04a
yymmm9 <46626991+yymmm9@users.noreply.github.com>
2021-03-25 13:23:41 +0100

    Update school.js

--- pages/school.js
+++ pages/school.js
@@ -7,23 +7,23 @@ import { useUser } from '@/utils/useUser';
 import { useRouter } from 'next/router';
 
 const School = () => {
-    const [postData, setPost] = useState(null);
-    const router = useRouter();
-      useEffect(()=>{
-          sanityClient.fetch(`*[_type == "post"]{
-              title,
-              slug,
-              mainImage{
-                  asset->{
-                      _id,
-                      url
-                  },
-                  alt
-              }
-          }`)
-          .then((data) => setPost(data))
-          .catch(console.error)
-      }, []);
+    // const [postData, setPost] = useState(null);
+    // const router = useRouter();
+    //   useEffect(()=>{
+    //       sanityClient.fetch(`*[_type == "post"]{
+    //           title,
+    //           slug,
+    //           mainImage{
+    //               asset->{
+    //                   _id,
+    //                   url
+    //               },
+    //               alt
+    //           }
+    //       }`)
+    //       .then((data) => setPost(data))
+    //       .catch(console.error)
+    //   }, []);
     
       return(
           <main>
@@ -31,19 +31,7 @@ const School = () => {
                   <h1></h1>
                   <h2></h2>
                   <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
-                      {postData && postData.map((post, index) => (
-                      <article>
-                          <Link to={"/post/" + post.slug.current} key={post.slug.current}>
-                            <span className="block h-64 relative rounded shadow" key={index}>
-                                <img src={post.mainImage.asset.url} alt={post.mainImage.alt}
-                                className="w-full h-full rounded-r object-cover absolute"/>
-                                <span className="block relative h-full flex jusitify-end items-end pr-4 pb-4">
-                                    <h3 className="">{post.title}</h3>
-                                </span>
-                            </span>
-                          </Link>
-                      </article>
-                      ))}
+                     
                   </div>
               </section>
           </main>
