86f191388f395a3503dd44ed2a77369d88575e3b
yymmm9 <46626991+yymmm9@users.noreply.github.com>
2021-03-25 15:28:10 +0100

    Update client.js

--- utils/client.js
+++ utils/client.js
@@ -2,5 +2,6 @@ import sanityClient from '@sanity/client'
 
 export default sanityClient({
     projectId: "efvz7syw",
-    dataset: "production"
+    dataset: "production",
+    apiVersion: '2021-03-25'
 })
