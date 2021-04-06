068f517cc009169b6ee083a8056d33a1ee452570
yymmm9 <46626991+yymmm9@users.noreply.github.com>
2021-04-06 13:16:35 +0200

    debug + alipay

--- pages/school.js
+++ pages/school.js
@@ -7,43 +7,36 @@ import React, { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import { useUser } from '@/utils/useUser';
 
-function School(){
-    
-    state={
-        fetchedLessons
-    }
+export default function School(){
 
-    async componentDidMount(){
+    // async componentDidMount(){
 
-        const lessonsRes = await axios({
-            metod: 'GET',
-            url: 'http://localhost:1337/lessons',
-        })
+    //     const lessonsRes = await axios({
+    //         metod: 'GET',
+    //         url: 'http://localhost:1337/lessons'
+    //     })
 
-        console.log("App. Cdidmount", lessonsRes)
+    //     console.log("App. Cdidmount", lessonsRes)
 
-        const fetchedLessons = lessonsRes.data;
+    //     const fetchedLessons = lessonsRes.data;
 
-        console.log("App did mount")
-    }
+    //     console.log("App did mount")
+    // }
 
     render(){
-        const {fetchedLessons} = this.state
+        // const {fetchedLessons} = this.state
 
-        render(
+        return(
 
             <main>
                 <section>
-                    <h1></h1>
+                    <h1>Start your journey</h1>
                     <h2></h2>
 
-                   <LessonTable lessons={fetchedLessons} />
+                   {/* <LessonTable lessons={fetchedLessons} /> */}
 
                 </section>
             </main>
       )
     }
 }
-
-
-export default School;
